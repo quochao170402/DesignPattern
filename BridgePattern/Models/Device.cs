5228e37db02926a5537888836906653914174114
quochao <quochao170402@gmail.com>
2024-01-15 17:24:32 +0700

    init project bridge pattern

---
+++ BridgePattern/Models/Device.cs
@@ -0,0 +1,10 @@
+﻿namespace BridgePattern;
+
+public interface IDevice
+{
+    string Name { get; set; }
+    public bool IsOn { get; set; }
+    void On();
+    void Off();
+    void Display();
+}
---
+++ BridgePattern/Models/TV.cs
@@ -0,0 +1,19 @@
+﻿namespace BridgePattern;
+
+public class TV : IDevice
+{
+    public void Display()
+    {
+        System.Console.WriteLine($"TV Display");
+    }
+
+    public void Off()
+    {
+        System.Console.WriteLine("TV off");
+    }
+
+    public void On()
+    {
+        System.Console.WriteLine("TV on");
+    }
+}
