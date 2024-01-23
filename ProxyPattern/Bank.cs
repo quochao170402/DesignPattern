27cddf19572c251d3c7cd2cc7458ca59b808074b
quochao <quochao170402@gmail.com>
2024-01-23 15:24:49 +0700

    implement proxy pattern

---
+++ ProxyPattern/Bank.cs
@@ -0,0 +1,44 @@
+﻿namespace ProxyPattern;
+
+public interface IBank
+{
+    void Deposit(int amount);
+    void Withdraw(int amount);
+}
+
+public class Bank : IBank
+{
+    public void Deposit(int amount)
+    {
+        System.Console.WriteLine($"Bank deposit {amount}");
+    }
+
+    public void Withdraw(int amount)
+    {
+        System.Console.WriteLine($"Bank withdraw {amount}");
+    }
+}
+
+public class CreditCard : IBank
+{
+    private readonly IBank _bank;
+
+
+    public CreditCard(IBank bank)
+    {
+        _bank = bank;
+    }
+
+    public void Deposit(int amount)
+    {
+        System.Console.WriteLine("Validate bank account");
+        _bank.Deposit(amount);
+    }
+
+    public void Withdraw(int amount)
+    {
+        System.Console.WriteLine("Validate bank account");
+        System.Console.WriteLine("Validate total amount");
+        _bank.Withdraw(amount);
+    }
+}
---
+++ ProxyPattern/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProxyPattern;
+
+Console.WriteLine("Proxy pattern");
+
+IBank proxy = new CreditCard(new Bank());
+proxy.Deposit(1000);
+proxy.Withdraw(100);
