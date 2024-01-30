7430b0766f23de934598b5e8f2dacb1c51c3e397
quochao <quochao170402@gmail.com>
2024-01-30 14:38:52 +0700

    Modify command implementation

--- CommandPattern/Invoker.cs
+++ CommandPattern/Invoker.cs
@@ -2,21 +2,43 @@
 
 public class Invoker
 {
-    private readonly ICommand _command;
+    private readonly Queue<ICommand> _commands;
+    private readonly Queue<ICommand> _histories;
 
-    public Invoker(ICommand command)
+    public Invoker()
     {
-        _command = command;
+        _commands = new Queue<ICommand>();
+        _histories = new Queue<ICommand>();
+    }
+
+    public void Push(ICommand command)
+    {
+        _commands.Enqueue(command);
     }
 
     public void Execute()
     {
-        _command.Execute();
+        if (_commands.Count > 0)
+        {
+            var command = _commands.Dequeue();
+            _histories.Enqueue(command);
+            Console.WriteLine($"Execute command {command.GetType()}");
+            command.Execute();
+        }
     }
 
     public void Undo()
     {
-        _command.Undo();
+        if (_histories.Count > 0)
+        {
+            var command = _histories.Dequeue();
+            Console.WriteLine("Command undo");
+            command.Undo();
+        }
+        else
+        {
+            Console.WriteLine("Command cannot undo");
+        }
     }
 
 }
--- CommandPattern/Program.cs
+++ CommandPattern/Program.cs
@@ -4,9 +4,32 @@ using CommandPattern;
 Console.WriteLine("Hello, World!");
 
 Receiver receiver = new Fan();
-Invoker invoker = new Invoker(new Command(receiver));
+Receiver tv = new TV();
+Receiver washingMachine = new WashingMachine();
+Receiver airConditioner = new AirConditioner();
+
+System.Console.WriteLine("Before");
+System.Console.WriteLine(receiver.State);
+System.Console.WriteLine(tv.State);
+System.Console.WriteLine(washingMachine.State);
+System.Console.WriteLine(airConditioner.State);
+
+Invoker invoker = new Invoker();
+
+invoker.Push(new Command(receiver));
+invoker.Push(new Command(tv));
+invoker.Push(new Command(washingMachine));
+invoker.Push(new Command(airConditioner));
+
+invoker.Execute();
+invoker.Execute();
+invoker.Execute();
 invoker.Execute();
 invoker.Undo();
-invoker.Execute();
-invoker.Execute();
-invoker.Execute();
+invoker.Undo();
+
+System.Console.WriteLine("After");
+System.Console.WriteLine(receiver.State);
+System.Console.WriteLine(tv.State);
+System.Console.WriteLine(washingMachine.State);
+System.Console.WriteLine(airConditioner.State);
