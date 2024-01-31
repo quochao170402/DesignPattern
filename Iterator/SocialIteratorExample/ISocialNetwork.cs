b6ac6a7acd8819f6d058d49f707bce08c004ca1a
quochao <quochao170402@gmail.com>
2024-01-31 17:31:22 +0700

    Implement iterator pattern

---
+++ Iterator/SocialIteratorExample/ISocialNetwork.cs
@@ -0,0 +1,66 @@
+﻿namespace IteratorPattern;
+
+public interface ISocialNetwork
+{
+    IProfileIterator CreateFriendsIterator(string profileId);
+    IProfileIterator CreateCoworkersIterator(string profileId);
+}
+
+public class Facebook : ISocialNetwork
+{
+    private readonly List<Profile> _profiles;
+
+    public Facebook(List<Profile> profiles)
+    {
+        if (profiles.Any())
+        {
+            _profiles = profiles;
+        }
+        else
+        {
+            _profiles = new List<Profile>();
+        }
+    }
+
+    private Profile? FindProfileByEmail(string email)
+    {
+        return _profiles.Find(x => x.Email == email);
+    }
+
+
+    public IProfileIterator CreateCoworkersIterator(string profileId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IProfileIterator CreateFriendsIterator(string profileId)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Instagram : ISocialNetwork
+{
+    public IProfileIterator CreateCoworkersIterator(string profileId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IProfileIterator CreateFriendsIterator(string profileId)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class LinkedIn : ISocialNetwork
+{
+    public IProfileIterator CreateCoworkersIterator(string profileId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IProfileIterator CreateFriendsIterator(string profileId)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Iterator/SocialIteratorExample/Profile.cs
@@ -0,0 +1,8 @@
+﻿namespace IteratorPattern;
+
+public class Profile
+{
+    public string Id { get; set; }
+    public string Email { get; set; }
+    public string Name { get; set; }
+}
