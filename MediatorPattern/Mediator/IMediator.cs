83dcfac80d4bdf1bc8855c0544480d54f1031b59
Quốc Hào <quochao170402@gmail.com>
2024-02-19 06:53:34 +0700

    add mediator\

---
+++ MediatorPattern/Mediator/IMediator.cs
@@ -0,0 +1,8 @@
+﻿namespace MediatorPattern;
+public interface IMediator
+{
+    void Get();
+    void Create();
+    void Update();
+    void Delete();
+}
---
+++ MediatorPattern/Mediator/Mediator.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace MediatorPattern;
+public class Mediator : IMediator
+{
+    public void Create()
+    {
+        System.Console.WriteLine("Create");
+    }
+
+
+    [Benchmark]
+    public void Delete()
+    {
+        System.Console.WriteLine("Delete");
+    }
+
+
+    public void Get()
+    {
+        System.Console.WriteLine("Get");
+    }
+
+    public void Update()
+    {
+        System.Console.WriteLine("Update");
+    }
+}
